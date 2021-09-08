561d9ed7a59bb840056af55904c9059e3c24fcac
ELECTRONICA 2021 EESTN°6 <89501984+EESTN62021@users.noreply.github.com>
2021-09-08 13:01:29 -0300

    Update PROGRAMA_simulador_2_JOYSTICKS.ino

--- PROGRAMA_simulador_2_JOYSTICKS.ino
+++ PROGRAMA_simulador_2_JOYSTICKS.ino
@@ -1,118 +1,165 @@
-//Arduino Joystick 2.0 Library, by MHeironimus (https://github.com/MHeironimus)
-//Beginners Guide, by Daniel Cantore
-//Example Code (Oct 2020), with in-depth commenting 
+#include <Gamepad.h>
 
-//Initial Definitions and Setup
-//Libary Inclusion
-#include <Joystick.h>
+int rightXcenter = 500;
+int rightYcenter = 500;
+int leftXcenter = 500;
+int leftYcenter = 500;
+double multiplierRX = 0.254; //127 / 500
+double multiplierRY = 0.254;
+double multiplierLX = 0.254;
+double multiplierLY = 0.254;
 
-//Define and Allocate Input Pins to memorable names
-#define joyX A0
-#define joyY A1
-#define joyRZ A3
-#define joyThrottle A2
-#define joyButton1 9
-#define joyButton2 8
-#define joyButton3 7
-
-//Initializing Axis as Integers, at a 0 default value
-int xAxis_ = 0;
-int yAxis_ = 0;
-int rzAxis_ = 0;
-int throttle_ = 0;
-
-//Setting up Buttons
-//Updating a static variable gives greater stability than reading directly from the digital pin.
-//Giving Default Values to the Buttons for later use
-  int lastButton1State = 0;
-  int lastButton2State = 0;
-  int lastButton3State = 0;
-
-//Defining the Joystick
-//The Joystick is defined in the following setup:
-//Joystick(Joystick HID ID, Joystick Type, Button Count, Hat Switch Count, Include X, Include Y, Include Z, Include Rx, Include Ry, Include Rz, Include Rudder, Include Throttle, Include Accelerator, Include Brake, Include Steering
-//Joystick HID ID: A Hex value identifier for HID Device Recognition (default: 0x03). DO NOT USE 0x01 or 0x02
-//Joystick type: Define the type of joystick from the types supported. Types: DEFAULT Joystick (0x04 or JOYSTICK_TYPE_JOYSTICK), Gamepad (0x05 or JOYSTICK_TYPE_GAMEPAD), Multi-Axis Controller (0x08 or JOYSTICK_TYPE_MULTI_AXIS)
-//Button Count: Number of Buttons shown to HID system (default: 32)
-//Hat Switch Count: Number of Hat Switches, max 2. (default:2)
-//Include X Axis: Determines whether the X axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Y Axis: Determines whether the Y axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Z Axis: Determines whether the Z axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Rx Axis: Determines whether the X Rotational axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Ry Axis: Determines whether the Y Rotational axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Rz Axis: Determines whether the Z Rotational axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Rudder: Determines whether a Rudder axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Throttle: Determines whether a Throttle axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Accelerator: Determines whether an Accelerator axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Brake: Determines whether a Brake axis is avalible for used by the HID system, defined as a bool value (default:true)
-//Include Steering: Determines whether a Steering axis is avalible for used by the HID system, defined as a bool value (default:true)
-
-Joystick_ Joystick(0x12, JOYSTICK_TYPE_JOYSTICK, 3, 0,true,true,false,false,false,true,false,true,false,false,false);
-
-//Set Auto Send State
-//Enables Auto Sending, allowing the controller to send information to the HID system, rather than waiting to be asked.
-const bool initAutoSendState = true;
+Gamepad gp;
 
 void setup() {
-  //Initialize Buttons
-  //Buttons set up between Digital Pin and Ground, following pin allocations from earlier on
-  pinMode(joyButton1, INPUT_PULLUP);
-  pinMode(joyButton2, INPUT_PULLUP);
-  pinMode(joyButton3, INPUT_PULLUP);
 
-  //Start Joystick - Needed to start the Joystick function libary
-  Joystick.begin();
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(14,  INPUT_PULLUP); //UPPER RIGHT
+  pinMode(15,  INPUT_PULLUP); //UPPER LEFT
+  pinMode(1,  INPUT_PULLUP); //LEFTBUTTON
+  pinMode(0,  INPUT_PULLUP); //RIGHTBUTTON
+  pinMode(4,  INPUT_PULLUP); //UP
+  pinMode(5,  INPUT_PULLUP); //DOWN
+  pinMode(6,  INPUT_PULLUP); //LEFT
+  pinMode(7,  INPUT_PULLUP); //RIGHT
+  pinMode(8,  INPUT_PULLUP); //Y
+  pinMode(9,  INPUT_PULLUP); //X
+  pinMode(10,  INPUT_PULLUP); //A
+  pinMode(16,  INPUT_PULLUP); //B  
+  
+  calibrate();
 }
 
 void loop() {
+  int lx, ly, rx, ry;
+  lx = analogRead(A3);
+  ly = analogRead(A2);
+  rx = analogRead(A1);
+  ry = analogRead(A0);
+  //we need to convert a 0-1000 to -127 - 127
+  lx = floor((lx - leftXcenter) * multiplierLX);
+  ly = floor((ly - leftYcenter) * multiplierLY);
+  rx = floor((rx - rightXcenter) * multiplierRX);
+  ry = floor((ry - rightYcenter) * multiplierRY);
+  if(lx > 127) lx = 127;
+  if(ly > 127) ly = 127;
+  if(rx > 127) rx = 127;
+  if(ry > 127) ry = 127;
+  gp.setLeftXaxis(lx);
+  gp.setRightXaxis(rx);
+  gp.setLeftYaxis(ly);
+  gp.setRightYaxis(ry);
   
-  //Axis Reading during Runtime
-  //Setting Read functions for each axis and parsing correctly. The X axis will be used as an example for explanation
-
-  //Reading the X Axis analog pin to the xAxis_ variable for processing
-  xAxis_ = analogRead(joyX);
-  //Mapping the X Axis data from a 0-1023 to 0-255 range for a smoother action
-  xAxis_ = map(xAxis_,0,1023,0,255);
-  //Set the Joystick X Axis value as the new, smoother, value
-  Joystick.setXAxis(xAxis_);
-
-  yAxis_ = analogRead(joyY);
-  yAxis_ = map(yAxis_,0,1023,0,255);
-  Joystick.setYAxis(yAxis_);
-
-  rzAxis_ = analogRead(joyRZ);
-  rzAxis_ = map(rzAxis_,0,1023,0,255);
-  Joystick.setRzAxis(rzAxis_);
-
-  throttle_ = analogRead(joyThrottle);
-  throttle_ = map(throttle_,0,1023,0,255);
-  Joystick.setThrottle(throttle_);
+  int UPLEFT, UPRIGHT, UP, DOWN, LEFT, RIGHT, RIGHTBUTTON, LEFTBUTTON, X, Y, A, B;
+  UPLEFT = digitalRead(14);
+  UPRIGHT = digitalRead(15);
+  RIGHTBUTTON = digitalRead(0);
+  LEFTBUTTON = digitalRead(1);
+  UP = digitalRead(4);
+  DOWN = digitalRead(5);
+  LEFT = digitalRead(6);
+  RIGHT = digitalRead(7);
+  X = digitalRead(8);
+  Y = digitalRead(9);
+  A = digitalRead(10);
+  B = digitalRead(16);
   
-  //Button Reading during Runtime
-  //Setting Read functions for each button, using a state value for memory. Button 1 will be used as an example for explanation
+      
+  
+  if(UPLEFT == LOW)    //
+    gp.setButtonState(0, true);
+  else
+    gp.setButtonState(0, false);
 
-  //Reading the current Button digital pin to the Current Button State for processing
-  int currentButton1State = !digitalRead(joyButton1);
-  //If loop - Check that the button has actually changed.
-  if (currentButton1State != lastButton1State){
-    //If the button has changed, set the specified HID button to the Current Button State
-    Joystick.setButton(0, currentButton1State);
-    //Update the Stored Button State
-    lastButton1State = currentButton1State;
-  }
+  if(UPRIGHT == LOW)
+    gp.setButtonState(1, true);
+  else
+    gp.setButtonState(1, false);
+ 
+  if(UP == LOW)
+    gp.setButtonState(2, true);
+  else
+    gp.setButtonState(2, false);
 
-    int currentButton2State = !digitalRead(joyButton2);
-  if (currentButton2State != lastButton2State){
-    Joystick.setButton(1, currentButton2State);
-    lastButton2State = currentButton2State;
-  }
-    int currentButton3State = !digitalRead(joyButton3);
-  if (currentButton3State != lastButton3State){
-    Joystick.setButton(2, currentButton3State);
-    lastButton3State = currentButton3State;
-  }
+  if(DOWN == LOW)
+    gp.setButtonState(3, true);
+  else
+    gp.setButtonState(3, false);    
 
-//Pole Delay/Debounce
-//To reduce unessecary processing, the frequency of the reading loop is delayed. The value(in ms) can be changed to match requirement
-delay(10);
+  if(LEFT == LOW)
+    gp.setButtonState(4, true);
+  else
+    gp.setButtonState(4, false); 
+
+  if(RIGHT == LOW)
+    gp.setButtonState(5, true);
+  else
+    gp.setButtonState(5, false);     
+
+  if(RIGHTBUTTON == LOW)
+    gp.setButtonState(6, true);
+  else
+    gp.setButtonState(6, false);   
+
+  if(LEFTBUTTON == LOW)
+    gp.setButtonState(7, true);
+  else
+    gp.setButtonState(7, false);     
+
+  if(X == LOW)
+    gp.setButtonState(8, true);
+  else
+    gp.setButtonState(8, false);
+
+  if(Y == LOW)
+    gp.setButtonState(9, true);
+  else
+    gp.setButtonState(9, false);
+
+  if(A == LOW)
+    gp.setButtonState(10, true);
+  else
+    gp.setButtonState(10, false);  
+
+  if(B == LOW)
+    gp.setButtonState(11, true);
+  else
+    gp.setButtonState(11, false);       
+
+  delay(20);
+}
+
+void calibrate()
+{
+  int lx, ly, rx, ry;
+  int i = 0;
+  while(i < 13)
+  {
+    lx = analogRead(A3);
+    ly = analogRead(A2);
+    rx = analogRead(A1);
+    ry = analogRead(A0);
+    bool validLX = lx > (leftXcenter - 100) && lx < (leftXcenter + 100);
+    bool validLY = ly > (leftYcenter - 100) && ly < (leftYcenter + 100);
+    bool validRX = rx > (rightXcenter - 100) && rx < (rightXcenter + 100);
+    bool validRY = ry > (rightYcenter - 100) && ry < (rightYcenter + 100);
+    if(validLX && validLY && validRX && validRY)
+    {
+      i++;
+      //nothing to do here!
+    }
+    else i = 0;
+    delay(20);
+  }
+  leftXcenter = lx;
+  leftYcenter = ly;
+  rightXcenter = rx;
+  rightYcenter = ry;
+  multiplierLX = (double)127 / (double)lx;
+  multiplierLY = (double)127 / (double)ly;
+  multiplierRX = (double)127 / (double)rx;
+  multiplierRY = (double)127 / (double)ry;
 }
